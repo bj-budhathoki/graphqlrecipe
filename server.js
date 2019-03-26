81278b6329bad378b09d84a7d68a55dce0b06b92
bj-budhathoki <bijayabudhathoki98@gmail.com>
2019-03-26 23:34:17 +0545

    token

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config({ path: "variables.env" });
 const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const Recipe = require("./models/Recipe");
 const User = require("./models/User");
 
@@ -12,21 +13,12 @@ const { resolvers } = require("./resolvers");
 
 //connects to databse
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, reconnectTries: 86400 })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    reconnectTries: 86400
+  })
   .then(() => console.log("database connected"))
   .catch(err => console.log(err));
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: { Recipe, User },
-  playground: {
-    endpoint: `http://localhost:4000/graphql`,
-    settings: {
-      "editor.theme": "dark"
-    }
-  }
-});
 
 //Initialize application
 const app = express();
@@ -36,6 +28,33 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+//set up jwt authentication middleware
+app.use(async (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (token !== "null") {
+    try {
+      const currentUser = await jwt.verify(token, process.env.SECRET);
+      req.currentUser = currentUser;
+      console.log(currentUser);
+    } catch (error) {
+      console.log(errror);
+    }
+  }
+  next();
+});
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ currentUser }) => ({ Recipe, User, currentUser }),
+  playground: {
+    endpoint: `http://localhost:4000/graphql`,
+    settings: {
+      "editor.theme": "dark"
+    }
+  }
+});
 server.applyMiddleware({ app });
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
